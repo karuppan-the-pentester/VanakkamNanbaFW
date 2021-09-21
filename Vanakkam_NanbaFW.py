590aed63546752b1b58e82cbc23786b69c105aae
Karuppasamy K <karuppan.the.pentester@gmail.com>
2021-09-21 22:49:09 +0530

    Animation Fixes

--- Vanakkam_NanbaFW.py
+++ Vanakkam_NanbaFW.py
@@ -9,36 +9,23 @@ def ProgramStartingAnimation():
     '''
     This Function Provides You an animation Like Metasploit starting animation
     '''
-    load_str = "Starting Vanakkam Nanba FrameWork.... "
-    ls_len = len(load_str)
-    animation = "|/-\\"
-    anicount = 0
-    counttime = 0
-    i = 0
-    while (counttime != 100):
-        time.sleep(0.075)
-        load_str_list = list(load_str)
-        x = ord(load_str_list[i])
-        y = 0
-        if x != 32 and x != 46:
-            if x > 90:
-                y = x-32
-            else:
-                y = x + 32
-            load_str_list[i] = chr(y)
-        res = ''
-        for j in range(ls_len):
-            res = res + load_str_list[j]
-        sys.stdout.write("\r"+res + animation[anicount])
-        sys.stdout.flush()
-        load_str = res
-        anicount = (anicount + 1) % 4
-        i = (i + 1) % ls_len
-        counttime = counttime + 1
-    # if os.name == "nt":
-    #     os.system("cls")
-    # else:
-    #     os.system("clear")
+    os.system('clear')
+    lowerstr = "[*] Starting Vanakkam Nanba FrameWork.... "
+    upperstr = lowerstr.upper()
+
+    for x in range(len(lowerstr)-4):
+        time.sleep(0.05)
+        sys.stdout.write('\r'+lowerstr[0:x] +
+                         upperstr[x] + lowerstr[x+1:] + ' |'+'\r')
+        time.sleep(0.05)
+        sys.stdout.write('\r'+lowerstr[0:x] +
+                         upperstr[x] + lowerstr[x+1:] + ' /'+'\r')
+        time.sleep(0.05)
+        sys.stdout.write('\r'+lowerstr[0:x] +
+                         upperstr[x] + lowerstr[x+1:] + ' -'+'\r')
+        time.sleep(0.05)
+        sys.stdout.write('\r'+lowerstr[0:x] +
+                         upperstr[x] + lowerstr[x+1:] + ' \\'+'\r')
 
 
 ProgramStartingAnimation()
