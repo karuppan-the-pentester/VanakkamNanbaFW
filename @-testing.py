ec704fbbca21ef1baaf424a916619aa42ada2b05
Karuppasamy K <karuppan.the.pentester@gmail.com>
2021-09-21 22:47:50 +0530

    Animation Fixed

--- @-testing.py
+++ @-testing.py
@@ -4,49 +4,18 @@ import time
 os.system('clear')
 lowerstr = "[*] Starting Vanakkam Nanba FrameWork.... "
 upperstr = lowerstr.upper()
-# for x in range(len(lowerstr)):
-#     s = '\r' + lowerstr[0:x] + upperstr[x] + lowerstr[x+1:] + '\r'
-#     sys.stdout.write(s)
-#     sys.stdout.flush()
-#     time.sleep(0.1)
+
 for x in range(len(lowerstr)-4):
-    # s = '\r' + lowerstr[0:x] + upperstr[x] + lowerstr[x+1:] + '\r'
-    # sys.stdout.write(s)
-    # sys.stdout.flush()
-    time.sleep(0.1)
+
+    time.sleep(0.05)
     sys.stdout.write('\r'+lowerstr[0:x] +
                      upperstr[x] + lowerstr[x+1:] + ' |'+'\r')
-    time.sleep(0.1)
+    time.sleep(0.05)
     sys.stdout.write('\r'+lowerstr[0:x] +
                      upperstr[x] + lowerstr[x+1:] + ' /'+'\r')
-    time.sleep(0.1)
+    time.sleep(0.05)
     sys.stdout.write('\r'+lowerstr[0:x] +
                      upperstr[x] + lowerstr[x+1:] + ' -'+'\r')
-    time.sleep(0.1)
+    time.sleep(0.05)
     sys.stdout.write('\r'+lowerstr[0:x] +
                      upperstr[x] + lowerstr[x+1:] + ' \\'+'\r')
-    # time.sleep(0.1)
-
-done = 'false'
-# here is the animation
-
-
-# def animate():
-#     for i in range(5):
-#         sys.stdout.write('\r'+lowerstr[0:x] +
-#                          upperstr[x] + lowerstr[x+1:] + ' |'+'\r')
-#         time.sleep(0.1)
-#         sys.stdout.write('\r'+lowerstr[0:x] +
-#                          upperstr[x] + lowerstr[x+1:] + ' /'+'\r')
-#         time.sleep(0.1)
-#         sys.stdout.write('\r'+lowerstr[0:x] +
-#                          upperstr[x] + lowerstr[x+1:] + ' -'+'\r')
-#         time.sleep(0.1)
-#         sys.stdout.write('\r'+lowerstr[0:x] +
-#                          upperstr[x] + lowerstr[x+1:] + ' \\'+'\r')
-#         time.sleep(0.1)
-
-
-# animate()
-# # long process here
-# done = 'true'
