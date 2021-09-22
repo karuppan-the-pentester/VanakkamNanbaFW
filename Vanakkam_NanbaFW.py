3d0644a87b6ad2bae491c63a1fa4ce34370e566c
Karuppasamy K <karuppan.the.pentester@gmail.com>
2021-09-22 15:37:45 +0530

    Banner Function

--- Vanakkam_NanbaFW.py
+++ Vanakkam_NanbaFW.py
@@ -26,6 +26,33 @@ def ProgramStartingAnimation():
         time.sleep(0.05)
         sys.stdout.write('\r'+lowerstr[0:x] +
                          upperstr[x] + lowerstr[x+1:] + ' \\'+'\r')
+    os.system('clear')
+
+
+def banner():
+    '''
+    This Function provides You Banner
+    '''
+    banners = [
+        '''
+                 ,           ,
+                /             \\
+               ((__---,,,---__))
+                  (_) O O (_)_________
+                     \ _ /            |\\
+                      o_o \   V N F   | \\
+                           \   _____  |  *
+                            |||   WW|||
+                            |||     |||
+
+
+       =[ vanakkam_nanba v1.0.0-dev                       ]
++ -- --=[ 1 exploits - 1 auxiliary - 1 post               ]
++ -- --=[ 0 payloads - 0 encoders  - 0 nops               ]
++ -- --=[ 0 evasion                                       ]
+''', ]
+    print(banners[0])
 
 
 ProgramStartingAnimation()
+banner()
--- cls-test.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-sys.stdout.write('\r Hi ')
-sys.stdout.flush()
